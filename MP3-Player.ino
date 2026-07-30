2c2e9d59a5bdf980bfaf58ab78755f7de457b77f
kara <kabrown118@gmail.com>
2026-07-30 16:12:27 -0500

    Updated state machine logic

--- MP3-Player.ino
+++ MP3-Player.ino
@@ -1,9 +1,9 @@
-
-/**next steps: set up & test mp3 module. design display. add pause/play and back buttons**/
+/* CANNOT UPLOAD CHANGES OR RUN UNTIL MILLIS CODE HAS BEEN FINALIZED */
+/**next steps: set up & test mp3 module. design display. add pause/play and prev buttons**/
 /**for the display:
 <now playing< (scrolling)
 song title (changes)
-back - pause/play - forward*/
+prev - pause/play - forward*/
 
 #include <SPI.h>
 #include <Wire.h>
@@ -16,22 +16,20 @@ back - pause/play - forward*/
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_HEIGHT 16
 #define LOGO_WIDTH 16
-int b_0 = 12;  //button defined pin
+unsigned long previousMillis;
+int b_next = 10;  //button defined pin
+int b_prev = 12;
 int i;
-//may need some defs here for sm
 
-enum { song0,
-	     song1,
-	     song2 };
+enum { play_song,
+	     next_song,
+	     prev_song };
 unsigned char songState;
-
-// static const unsigned char PROGMEM my_bitmap[] = {
-// };
 
 
 void setup() {
-	pinMode(b_0, INPUT_PULLUP);  //needs to be INPUT_PULLUP not just INPUT if not using resistor
-
+	pinMode(b_next, INPUT_PULLUP);  //needs to be INPUT_PULLUP not just INPUT if not using resistor
+	pinMode(b_prev, INPUT_PULLUP);  //needs to be INPUT_PULLUP not just INPUT if not using resistor
 	Serial.begin(9600);
 
 	if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -42,85 +40,79 @@ void setup() {
 	display.display();
 	delay(2000);  // Pause for 2 seconds
 	display.clearDisplay();
-	//testdrawbitmap();  // Draw a small bitmap image
+	previousMillis = millis();
 }
 
 void loop() {
-
-	int b_val = digitalRead(b_0);
+	int next_status = digitalRead(b_next);
+	int prev_status = digitalRead(b_prev);
+	unsigned long currentMillis = millis();
 
 	switch (songState) {
 
-		case song0:
-			delay(120);
-			i = 1;
-			funcdraw();
-			if (b_val == LOW) {
-				Serial.print("1");
-				Serial.print('\n');
-	
-				songState = song1;
-				break;
-			}
-			if (b_val == HIGH) {
-				Serial.print("wait");
-				Serial.print('\n');
-				songState = song0;
-				break;
+		case play_song:
+
+			if (next_status == LOW) {
+				songState = next_song;
+			} else if (prev_status == LOW) {
+				songState = prev_song;
 			}
 
-		case song1:
-			delay(120);
-			//print 2 on the screen
-			i = 2;
-			funcdraw();
-			if (b_val == LOW) {
-				Serial.print("2");
-				Serial.print('\n');
-	
-				songState = song2;
-				break;
+			else if (currentMillis - previousMillis >= 3600) {	//automatically continue to next song after current song is over
+				songState = next_song;
 			}
-			if (b_val == HIGH) {
-				Serial.print("wait");
-				Serial.print('\n');
-				songState = song1;
-				break;
-			}
+			break;
 
-		case song2:
+		case next_song:
 			delay(120);
-			//print 3 on the screen
-			i = 3;
+			next();
+			i++;
 			funcdraw();
-			if (b_val == LOW) {
-				Serial.print("3");
-				Serial.print('\n');
-	
-				songState = song0;
-				break;
-			}
-			if (b_val == HIGH) {
-				Serial.print("wait");
-				Serial.print('\n');
-				songState = song2;
-				break;
-			}
+			previousMillis = millis();
+			songState = play_song;
+			break;
+
+		case prev_song:
+			delay(120);
+			prev();
+			i = i - 1;
+			funcdraw();
+			previousMillis = millis();
+			songState = play_song;
+			break;
 	}
-	//Serial.println("Exited state machine.");
 }
 
-void testscrolltext(void) {
+void next() {		//will be defined accurately later for player
+	Serial.write("next song");
+	Serial.write('\n');
+}
+
+void prev() {		//will be defined accurately later for player
+	Serial.write("previous song");
+	Serial.write('\n');
+}
+
+void funcdraw() {
+	display.clearDisplay();
+	display.setTextSize(1);               // Normal 1:1 pixel scale
+	display.setTextColor(SSD1306_WHITE);  // Draw white text
+	display.setCursor(0, 0);              // Start at top-left corner
+	display.println(F("This is song "));
+	display.print(i);
+	display.display();
+}
+
+
+void testscrolltext(void) { //will be used for display later
 	display.clearDisplay();
 
 	display.setTextSize(2);  // Draw 2X-scale text
 	display.setTextColor(SSD1306_WHITE);
 	display.setCursor(10, 0);
-	display.println(F("DVD"));
+	display.println(F("Now playing"));
 	display.display();  // Show initial text
 	delay(100);
-
-	// Scroll in various directions, pausing in-between:
 	display.startscrollright(0x00, 0x0F);
 	delay(2000);
 	display.stopscroll();
@@ -129,34 +121,6 @@ void testscrolltext(void) {
 	delay(2000);
 	display.stopscroll();
 	delay(1000);
-	display.startscrolldiagright(0x00, 0x07);
-	delay(2000);
-	display.startscrolldiagleft(0x00, 0x07);
-	delay(2000);
-	display.stopscroll();
-	delay(1000);
+
 }
 
-// void testdrawbitmap(void) {
-//   display.clearDisplay();
-
-//   display.drawBitmap(
-//     /*display.width() - LOGO_WIDTH) / 2*/ 0,
-//     /*display.height() - LOGO_HEIGHT) / 2*/ 0,
-//     my_bitmap, SCREEN_WIDTH, (SCREEN_HEIGHT*2), 1);
-//   display.display();
-//   delay(1000);
-// }
-
-// void testdrawbitmap2(void) {
-//   display.clearDisplay();
-void funcdraw(void) {
-	display.clearDisplay();
-
-	display.setTextSize(1);               // Normal 1:1 pixel scale
-	display.setTextColor(SSD1306_WHITE);  // Draw white text
-	display.setCursor(0, 0);              // Start at top-left corner
-	display.println(F("This is song "));
-	display.print(i);
-	display.display();
-}
