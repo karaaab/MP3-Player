eb5839d40895f56ac24ca4e0e3c5105393c2f6c1
kara <kabrown118@gmail.com>
2026-08-09 23:42:43 -0500

    Added pause button functionality

--- MP3-Player.ino
+++ MP3-Player.ino
@@ -1,9 +1,3 @@
-/*next step: add pause/play functionality*/
-/**for the display:
-<now playing< (scrolling)
-song title (changes)
-prev - pause/play - forward*/
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -19,65 +13,71 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_HEIGHT 16
 #define LOGO_WIDTH 16
 unsigned long previousMillis;
+unsigned long pauseMillis;
 int b_next = 10;  //button defined pin
+int b_pauseplay = 11;
 int b_prev = 12;
 int i;
 enum { play_song,
+	     pause_song,
 	     next_song,
 	     prev_song };
 unsigned char songState;
 YX5300_ESP32 mp3;
-
 void setup() {
 	mp3 = YX5300_ESP32(Serial2, RX, TX);  //init yx5300 connection
 	pinMode(b_next, INPUT_PULLUP);        //needs to be INPUT_PULLUP not just INPUT if not using resistor
 	pinMode(b_prev, INPUT_PULLUP);        //needs to be INPUT_PULLUP not just INPUT if not using resistor
+	pinMode(b_pauseplay, INPUT_PULLUP);
 	Serial.begin(9600);
-
 	if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
 		Serial.println(F("SSD1306 allocation failed"));
 		for (;;)
 			;
 	}
-
 	display.display();
 	delay(2000);  // Pause for 2 seconds
 	display.clearDisplay();
 	previousMillis = millis();
+	pauseMillis = millis();
 }
 
 void loop() {
 	int next_status = digitalRead(b_next);
+	int pause_status = digitalRead(b_pauseplay);
 	int prev_status = digitalRead(b_prev);
 	unsigned long currentMillis = millis();
 
-	Serial.write("We are now entering state machine.");
-	Serial.write('\n');
 	switch (songState) {
-
 		case play_song:
-			Serial.write("case play song");
 			delay(200);
 			if (currentMillis - previousMillis < 60000) {
-				Serial.write("looking");
-				Serial.write('\n');
 				if (next_status == LOW) {
-					Serial.write("going to next song");
-					Serial.write('\n');
 					delay(120);
 					songState = next_song;
-
+				} else if (pause_status == LOW) {
+					songState = pause_song;
 				} else if (prev_status == LOW) {
 					songState = prev_song;
 				}
 			} else if (currentMillis - previousMillis >= 60000) {  //automatically continue to next song after current song is over
 				songState = next_song;
+			}  //may not need this anymore given the new library I am using. will experiment further.
+			break;
+		case pause_song:
+			mp3.pause();
+			delay(120);
+			if (currentMillis > previousMillis) {  //universally true statement
+				if (pause_status == LOW) {
+					mp3.resume();
+					songState = play_song;
+				}
 			}
 			break;
 
 		case next_song:
-			Serial.write("next song case reached");
-			Serial.write('\n');
+			//Serial.write("next song case reached");
+			//Serial.write('\n');
 			delay(120);
 			mp3.next();
 			i++;
@@ -88,7 +88,6 @@ void loop() {
 			previousMillis = millis();
 			songState = play_song;
 			break;
-
 		case prev_song:
 			delay(120);
 			mp3.prev();
@@ -102,7 +101,6 @@ void loop() {
 			break;
 	}
 }
-
 void funcdraw() {
 	display.clearDisplay();
 	display.setTextSize(1);               // Normal 1:1 pixel scale
@@ -111,24 +109,4 @@ void funcdraw() {
 	display.println(F("This is song "));
 	display.print(i);
 	display.display();
-}
-
-
-void testscrolltext(void) {  //will be used for display later
-	display.clearDisplay();
-
-	display.setTextSize(2);  // Draw 2X-scale text
-	display.setTextColor(SSD1306_WHITE);
-	display.setCursor(10, 0);
-	display.println(F("Now playing"));
-	display.display();  // Show initial text
-	delay(100);
-	display.startscrollright(0x00, 0x0F);
-	delay(2000);
-	display.stopscroll();
-	delay(1000);
-	display.startscrollleft(0x00, 0x0F);
-	delay(2000);
-	display.stopscroll();
-	delay(1000);
 }
