87b63ad723f693f6314ef8077b6fdba760b4ca19
kara <kabrown118@gmail.com>
2026-08-11 17:50:15 -0500

    Volume controls

--- MP3-Player.ino
+++ MP3-Player.ino
@@ -17,18 +17,23 @@ unsigned long pauseMillis;
 int b_next = 10;  //button defined pin
 int b_pauseplay = 11;
 int b_prev = 12;
+int b_vinc = 4;
+int b_vdec = 3;
 int i;
 enum { play_song,
 	     pause_song,
 	     next_song,
-	     prev_song };
+	     prev_song,
+	     volume_ctrl };
 unsigned char songState;
 YX5300_ESP32 mp3;
 void setup() {
 	mp3 = YX5300_ESP32(Serial2, RX, TX);  //init yx5300 connection
 	pinMode(b_next, INPUT_PULLUP);        //needs to be INPUT_PULLUP not just INPUT if not using resistor
-	pinMode(b_prev, INPUT_PULLUP);        //needs to be INPUT_PULLUP not just INPUT if not using resistor
+	pinMode(b_prev, INPUT_PULLUP);
 	pinMode(b_pauseplay, INPUT_PULLUP);
+	pinMode(b_vinc, INPUT_PULLUP);
+	pinMode(b_vdec, INPUT_PULLUP);
 	Serial.begin(9600);
 	if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
 		Serial.println(F("SSD1306 allocation failed"));
@@ -40,13 +45,14 @@ void setup() {
 	display.clearDisplay();
 	previousMillis = millis();
 	pauseMillis = millis();
-	mp3.playFolderInLoop(01);
 }
 
 void loop() {
 	int next_status = digitalRead(b_next);
 	int pause_status = digitalRead(b_pauseplay);
 	int prev_status = digitalRead(b_prev);
+	int vinc_status = digitalRead(b_vinc);
+	int vdec_status = digitalRead(b_vdec);
 	unsigned long currentMillis = millis();
 
 	switch (songState) {
@@ -60,13 +66,24 @@ void loop() {
 					songState = pause_song;
 				} else if (prev_status == LOW) {
 					songState = prev_song;
+				} else if (vinc_status == LOW) {
+					mp3.incrementVolume();
+					Serial.write("up vol");
+					Serial.write('\n');
+					delay(100);
+				} else if (vdec_status == LOW) {
+					mp3.decrementVolume();
+					Serial.write("down vol");
+					Serial.write('\n');
+					delay(100);
 				}
 			}
 			break;
+
 		case pause_song:
 			mp3.pause();
 			delay(120);
-			if (currentMillis > previousMillis) {  //universally true statement
+			if (currentMillis > previousMillis) {
 				if (pause_status == LOW) {
 					mp3.resume();
 					songState = play_song;
@@ -75,8 +92,8 @@ void loop() {
 			break;
 
 		case next_song:
-			//Serial.write("next song case reached");
-			//Serial.write('\n');
+			Serial.write("next song case reached");
+			Serial.write('\n');
 			delay(120);
 			mp3.next();
 			i++;
@@ -87,6 +104,7 @@ void loop() {
 			previousMillis = millis();
 			songState = play_song;
 			break;
+
 		case prev_song:
 			delay(120);
 			mp3.prev();
